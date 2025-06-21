7da0a3dd6a652a60bdd0728c754e8df5e2a1ea20
Life Giver <lifefotsing@gmail.com>
2025-06-21 12:03:50 +0100

    Implementation of password-reset

--- server/app.js
+++ server/app.js
@@ -1,11 +1,16 @@
 import express from 'express'
-const app= express()
+const app = express()
 const port = 3000
 
 import registerRoute from './routes/register.js'
+import resetRoute from './routes/reset.js';
+
 app.use(express.json())
 app.use(registerRoute)
 
-app.listen(port, () =>{
+app.use(resetRoute);
+
+
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
