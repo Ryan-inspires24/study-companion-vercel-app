48e5f4ec91fefe776826a5a184c899074e7a1a6c
Ryan-inspires24 <kinsaryanabecklin@gmail.com>
2025-06-25 10:33:44 +0100

    first attempt to deploy backend on vercel

---
+++ api/app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import serverless from 'serverless-http';
+import authRoute from './routes/index.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(authRoute); 
+
+export const handler = serverless(app); 
