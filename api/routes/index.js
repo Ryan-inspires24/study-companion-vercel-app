9e275190769a96b8541fb621e580fa3c72de4c92
Ryan-inspires24 <kinsaryanabecklin@gmail.com>
2025-06-26 22:37:07 +0100

    10th attempt to deploy to vercel

--- api/routes/index.js
+++ api/routes/index.js
@@ -7,6 +7,6 @@ const router = express.Router();
 
 // Registration route
 router.post('/api/auth/register', register);
-router.post('/api/auth/reset-password', passwordReset);
+// router.post('/api/auth/reset-password', passwordReset);
 router.post('/api/auth/login', login)
 export default router;
