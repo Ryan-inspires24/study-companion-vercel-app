3d89b375bd62defa2ac95cc074942769972d8bf7
Ryan-inspires24 <kinsaryanabecklin@gmail.com>
2025-06-26 21:55:38 +0100

    5th attempt to deploy to vercel

--- src/handlers/firebase.js
+++ src/handlers/firebase.js
@@ -1,12 +1,12 @@
-import { initializeApp } from "firebase-admin";
+import { initializeApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-import { getFirestore } from "firebase/firestore"; 
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAMzVoxzc7mkP8AMJVXqzCGVkzm_3d7lKg",
   authDomain: "gce-study-companion.firebaseapp.com",
   projectId: "gce-study-companion",
-  storageBucket: "gce-study-companion.appspot.com", 
+  storageBucket: "gce-study-companion.appspot.com",
   messagingSenderId: "845951616251",
   appId: "1:845951616251:web:5826deba18c810a2c55c5e",
   measurementId: "G-Y9TZZ4B8YX"
@@ -15,5 +15,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const db = getFirestore(app); 
+export const db = getFirestore(app);
+
 export { createUserWithEmailAndPassword, signInWithEmailAndPassword };
