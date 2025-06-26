13ca5ca139806dcee3926433180df5f4176d5b79
Ryan-inspires24 <kinsaryanabecklin@gmail.com>
2025-06-26 21:42:10 +0100

    imported Initialize app from firebase-admin

--- src/handlers/firebase.js
+++ src/handlers/firebase.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase-admin";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"; 
 
