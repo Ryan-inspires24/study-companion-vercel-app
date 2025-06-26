67e7f7a0a4f01359912c7aa5a26293e1b21582ef
Ryan-inspires24 <kinsaryanabecklin@gmail.com>
2025-06-26 21:16:16 +0100

    Move app.js to server folder and update config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,20 +1,14 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
+import admin from 'firebase-admin';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAMzVoxzc7mkP8AMJVXqzCGVkzm_3d7lKg",
-  authDomain: "gce-study-companion.firebaseapp.com",
-  projectId: "gce-study-companion",
-  storageBucket: "gce-study-companion.firebasestorage.app",
-  messagingSenderId: "845951616251",
-  appId: "1:845951616251:web:5826deba18c810a2c55c5e",
-  measurementId: "G-Y9TZZ4B8YX"
-};
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
+  });
+}
 
-
-const app = initializeApp(firebaseConfig);
-export  const auth = getAuth(app);
-export {createUserWithEmailAndPassword, signInWithEmailAndPassword};
-
-const analytics = getAnalytics(app);
+const db = admin.firestore();
+export { db };
