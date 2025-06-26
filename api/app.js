bceee5829e84ef3b25808a022d362a3f43c5d960
Ryan-inspires24 <kinsaryanabecklin@gmail.com>
2025-06-26 22:11:16 +0100

    7th attempt to deploy to vercel

--- api/app.js
+++ api/app.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import serverless from 'serverless-http';
-import authRoute from './routes/index.js';
+// import authRoute from './routes/index.js';
 
 const app = express();
 
 app.use(express.json());
-app.get('/', (req, res) => {
+app.get('/health', (req, res) => {
   res.send('Hello from Express + serverless on Vercel!');
 });
 
-app.use(authRoute); 
+// app.use(authRoute); 
 
 export default serverless(app);
